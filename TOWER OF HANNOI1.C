308d2213aec48364139e199357cf80201a0ebe23
Mrsantanunandi <santanu2005nandi@gmail.com>
2024-09-09 15:33:02 +0530

    Create TOWER OF HANNOI1.C

---
+++ TOWER OF HANNOI1.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void TOH(int n,char source,char dest,char temp)
+{
+    if(n==1)
+    {
+        printf("\nMove %dst disk from %c to %c via %c",n,source,dest,temp);
+    }
+    else
+    {
+        TOH(n-1,source,temp,dest);
+        TOH(1,source,dest,temp);
+        TOH(n-1,temp,dest,source);
+    }
+}
+int main()
+{
+    int n;
+    char A,B,C;
+    printf("Eneter num of disk: ");
+    scanf("%d",&n);
+    TOH(n,'A','B','C');
+    return 0;
+}
